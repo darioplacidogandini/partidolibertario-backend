c3504dee75bae3ed2cfbd471de8b85a111d699df
darioplacidogandini <darioplacidogandini@outlook.com>
2022-07-29 02:12:50 -0300

    .

--- src/main/java/com/dariogandini/partidolibertariobackend/service/JwtUserDetailsService.java
+++ src/main/java/com/dariogandini/partidolibertariobackend/service/JwtUserDetailsService.java
@@ -14,7 +14,7 @@ public class JwtUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		if ("milei2023".equals(username)) {
-			return new User("milei2023", "$2y$10$qG1J0dpPbIGqwsP9EmiBbe5Z55lF15SxmE5X2dM7WZsqfpjneTp4a",
+			return new User("milei2023", "$2y$10$5T5W8Xn0eUG.TpNtAPC.mOy09RKph5ECAEKOxBaFZ3vBbmSH0kegS",
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
