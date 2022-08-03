bc649ff74f8ce81f3657ed0d6992831ac89c3c91
darioplacidogandini <darioplacidogandini@outlook.com>
2022-08-03 00:55:14 -0300

    .

--- src/main/java/com/dariogandini/partidolibertariobackend/controller/AfiliadosController.java
+++ src/main/java/com/dariogandini/partidolibertariobackend/controller/AfiliadosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,11 @@ public class AfiliadosController {
         Afiliados afiliados = afiliadosRepository.findById(id).orElse(null);
         return ResponseEntity.ok(afiliados);
     }
+
+    @PostMapping("/add")
+	public Afiliados add(@RequestBody Afiliados afiliados) {
+		return afiliadosRepository.save(afiliados);
+	}
 
     @PutMapping("/edit/{id}")
     public ResponseEntity<Afiliados> edit(@PathVariable Long id, 
